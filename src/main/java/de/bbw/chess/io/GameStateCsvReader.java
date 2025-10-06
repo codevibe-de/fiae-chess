b4e00d93792fc83909794a84ec4a570f1209e26d
Eric <ericbothe163@yahoo.de>
2025-10-06 10:27:24 +0200

    minor changes in GameStateCsvReader

--- src/main/java/de/bbw/chess/io/GameStateCsvReader.java
+++ src/main/java/de/bbw/chess/io/GameStateCsvReader.java
@@ -6,7 +6,6 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.List;
-import java.util.Objects;
 
 // CSV -> GameStateCsvReader -> GameState
 public class GameStateCsvReader {
@@ -60,18 +59,18 @@ public class GameStateCsvReader {
         // 5. letzte (neunte) Zeile lesen, muss "black" oder "white" enthalten
         // Es wird nur die neunte Zeile gelesen
         String lastRow = rows.get(8).trim();
-        String activePlayer = "";
-        String passivPlayer = "";
+        String activePlayer = " ";
+        String inactivePlayer = " ";
         if(lastRow.equalsIgnoreCase("white") || lastRow.equalsIgnoreCase("black")) {
             activePlayer = lastRow;
-            passivPlayer = activePlayer.equalsIgnoreCase("white") ? "black" : "white";
-        } else throw new GameStateFormatException("Inkorrekte/fehlende Spielerfarbe");
+            inactivePlayer = activePlayer.equalsIgnoreCase("white") ? "black" : "white";
+        } else throw new GameStateFormatException("Inkorrekte/fehlende Spielerfarbe: " + lastRow + "Erwartet: 'white'/'black'");
 
         // 6. merke, wer aktive spieler ist
         // Beide Spieler sind zur Weiterbenutzung verfügbar
         player1 = new Player(activePlayer);
         player1.setTurn(true);
-        player2 = new Player(passivPlayer);
+        player2 = new Player(inactivePlayer);
 
         return null; // todo
     }
