4ac184541a9ed4682a17f10ade07557967dc65e1
Thomas Auinger <thomas@auinger.de>
2025-10-06 08:29:05 +0200

    formatted code, removed import

--- src/main/test/de/bbw/chess/model/PositionTest.java
+++ src/main/test/de/bbw/chess/model/PositionTest.java
@@ -3,21 +3,19 @@ package de.bbw.chess.model;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class PositionTest {
 
     @Test
     void checkChessNotation() {
         // given - a Position with row/col coordinates
-        Position p = new Position(0,0);
+        Position p = new Position(0, 0);
         // when - asking for typical "chess coordinates"
         String coords = p.getSquareCoordinate();
         // then
         Assertions.assertEquals("a8", coords);
 
         // given - a Position with row/col coordinates
-        Position p2 = new Position(1,4);
+        Position p2 = new Position(1, 4);
         // when - asking for typical "chess coordinates"
         String coords2 = p2.getSquareCoordinate();
         // then
